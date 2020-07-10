caa3ca7aeaba384ea9716e6c7778a138e0558f64
Joe Keohan <joe.keohan@generalassemb.ly>
2020-07-10 16:36:14 -0400

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
-// 1. Require body-parser and save it to the variable parser.
 
 const app = express()
 
 const cookbookRouter = require('./controllers/cookbookRoutes')
 const authorRouter = require('./controllers/authorRoutes')
 
-// 2. Add the coded needed to make body-parser work within your app.
+// Add the middleware code needed to accept incoming data and add it to req.body
 
 app.use('/api/cookbooks/', cookbookRouter)
 app.use('/api/authors/', authorRouter)

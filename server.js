c48a0bcad9ef6932c6d12efebbc2b686d25d5293
Joe Keohan <joe.keohan@generalassemb.ly>
2020-10-16 08:23:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
-
 const app = express()
-
-const cookbookRouter = require('./controllers/cookbookRoutes')
-const authorRouter = require('./controllers/authorRoutes')
 
 // Add the middleware code needed to accept incoming data and add it to req.body
 
+const cookbookRouter = require('./controllers/cookbookRoutes')
 app.use('/api/cookbooks/', cookbookRouter)
+
+const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
 
 app.listen(4000, () => console.log('Server running on port 4000!'))
